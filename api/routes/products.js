ba87c2aa28959ca54c7d49487b3fbdcc96c261ba
Russell McCreath <russell.mccreath@phs.scot>
2021-09-07 22:26:15 +0100

    update HTTP status code for POST

--- api/routes/products.js
+++ api/routes/products.js
@@ -10,7 +10,7 @@ router.get("/", (req, res, next) => {
 
 // HTTP POST
 router.post("/", (req, res, next) => {
-  res.status(200).json({
+  res.status(201).json({
     message: "Handling POST requests to /products",
   });
 });
